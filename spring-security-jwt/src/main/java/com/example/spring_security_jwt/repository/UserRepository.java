ff9a845db0b6025210c29a3e409c4dad0bcd3d5d
hjh <ktorn21@naver.com>
2026-02-27 23:21:40 +0900

    회원정보 entity

---
+++ spring-security-jwt/src/main/java/com/example/spring_security_jwt/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.spring_security_jwt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.spring_security_jwt.entity.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer>{
+
+}
