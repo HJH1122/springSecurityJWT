cf94ebf534480b70afa483b487b4e4400e9a45dd
hjh <ktorn21@naver.com>
2026-03-02 14:11:57 +0900

    로그인 검증기능

---
+++ spring-security-jwt/src/main/java/com/example/spring_security_jwt/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.example.spring_security_jwt.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.spring_security_jwt.dto.CustomUserDetails;
+import com.example.spring_security_jwt.entity.UserEntity;
+import com.example.spring_security_jwt.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if(userData != null) {
+
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+    
+}
